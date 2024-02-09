ac5c5f39bcd60bd482dd8a319f197b75ad1ea7cc
Nicovied0 <briannoviedo@gmail.com>
2024-02-09 16:51:15 -0300

    add more styles

--- client/src/app/auth/auth-routing.module.ts
+++ client/src/app/auth/auth-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeviewsComponent } from '../home/views/homeviews/homeviews.component';
+import { AuthviewComponent } from './views/authview/authview.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeviewsComponent,
+    component: AuthviewComponent,
   },
 ];
 
